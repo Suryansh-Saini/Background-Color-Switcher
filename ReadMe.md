80512de3d311d478b31fef9a1fecd2e4f7eab40a
Suryansh-Saini <suryansh19saini@gmail.com>
2024-01-14 20:12:51 +0530

    Made js code smaller and Added ReadMe.md

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+It is a very small project made using HTML, CSS, Javascript.<br/>
+P.S. - It is my second Project and made for learning Frontend Development, through Project based Learning approach.
--- script.js
+++ script.js
@@ -2,17 +2,4 @@ const button = document.querySelectorAll('span')
 const body = document.querySelector('body')
 button.forEach(function (button) {
     button.addEventListener('click', function (event) {
-        if(event.target.id === 'blue') {
-            body.style.backgroundColor = 'blue'
-        }
-        if(event.target.id === 'yellow') {
-            body.style.backgroundColor = 'yellow'
-        }
-        if(event.target.id === 'orange') {
-            body.style.backgroundColor = 'orange'
-        }
-        if(event.target.id === 'red') {
-            body.style.backgroundColor = 'red'
-        }
-    })
-})
+        body.style.backgroundColor = event.target.id})})
